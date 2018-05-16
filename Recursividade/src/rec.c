024ee40248c1656c8665a0b140dc1a42f2e5d978
Igor Mattar <31900876+peppozx@users.noreply.github.com>
2018-05-16 11:05:11 -0300

    Add files via upload

--- Recursividade/src/rec.c
+++ Recursividade/src/rec.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void ex(int a);
-int f(int a,int b);
+int f(int a,int b,int res);
+void tabuada(int x, int y);
+void serie(int param, double *res);
 
 int main() {
 	setbuf(stdout, NULL);
-	ex(10);
-	printf("\n%d",f(3,5));
+	int res = f(3,5,0);
+	printf("%d",res);
 	return 0;
-	printf("\n");
 }
 
 void ex(int a) {
@@ -22,11 +24,28 @@ void ex(int a) {
 	ex(a-1);
 }
 
-int f(int a,int b) {
+int f(int a,int b,int res) {
 	if(b == 0) {
-		return 0;
+		return res;
+	}else {
+		return f(a,b-1,res+a);
 	}
-	return a+f(a,b-1);
 }
 //3 + (3 + (3 + (3 + (0)))
+void tabuada(int x,int y) {
+	if(y == 0) {
+		printf("\n%d * %d = %d",x,y,x*y);
+		return;
+	}
+	printf("\n%d * %d = %d",x,y,x*y);
+	tabuada(x,y-1);
+}
 
+void serie(int param,double *res) {
+	if(param == 0) {
+		return;
+	}else {
+		*res += (pow(param,3))/pow(10,param-1);
+		serie(param-1,res);
+	}
+}
