05252bbcdb8011b5d76b41844051e195921fd31b
matheusrm <39566134+matheusrm@users.noreply.github.com>
2018-05-23 17:22:43 -0300

    Update library.h

--- AutoInstrucional/library.h
+++ AutoInstrucional/library.h
@@ -1,1 +1,16 @@
+#ifndef  LIBARY_H
+#define  LIBARY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void nomeFun(char *nome);
+void nomeDep(char *nome);
+void cargoFuncionario(char *cargo);
+void quantDependente(int *quantDependentes);
+void salario(float *salario,char *cargo, int *quantDependentes);
+void idadeDependente(int *idade) ;
+int verificador (int cont,char *codigo, int resultado );
+void codigo(int *vet, char *nome[50], char matricula[9]);
+
+#endif 
